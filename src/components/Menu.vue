c7e3798286d8d693d9303f1542584cfe0138f98b
Askar <askar.karasaev@gmail.com>
2020-01-21 14:31:26 +0900

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template><div></div></template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          name: "Apple",
+          price: 20
+        },
+        {
+          name: "Orange",
+          price: 21
+        },
+        {
+          name: "Pear",
+          price: 22
+        },
+        {
+          name: "Grape",
+          price: 23
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
