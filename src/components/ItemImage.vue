fc8178f207a9bf22006ccf7096552925c3e77b2e
Askar <askar.karasaev@gmail.com>
2020-01-22 16:46:48 +0900

    Nesting 2 components in Menu using grid

---
+++ src/components/ItemImage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div><p>Hello from ItemImage component</p></div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <ItemsList />
+    <!-- Two columns -->
+    <div class="flex mb-4">
+      <div class="w-1/2 bg-gray-400">
+        <ItemsList />
+      </div>
+      <div class="w-1/2 bg-gray-500">
+        <ItemImage />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ItemsList from "./ItemsList";
+import ItemImage from "./ItemImage";
+
 export default {
   components: {
-    ItemsList
+    ItemsList,
+    ItemImage
   }
 };
 </script>
