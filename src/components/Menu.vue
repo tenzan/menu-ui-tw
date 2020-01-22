0cc2c2f394619d4854f832569b3ba9a8687e7d72
Askar <askar.karasaev@gmail.com>
2020-01-22 16:09:49 +0900

    Nesting ItemsList in Menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,71 +1,14 @@
 <template>
   <div>
-    <table class="border-separate">
-      <tbody
-        v-for="item in menuItems"
-        :key="item.name"
-        :class="item.highlight ? 'highlight' : ''"
-      >
-        <tr>
-          <td class="px-4 py-2 text-left">{{ item.name }}</td>
-          <td class="px-4 py-2">{{ item.price }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <ItemsList />
   </div>
 </template>
+
 <script>
+import ItemsList from "./ItemsList";
 export default {
-  data() {
-    return {
-      menuItems: [
-        {
-          name: "Apple",
-          price: 20,
-          image_url: "../assets/images/apple.jpg"
-        },
-        {
-          name: "Orange",
-          price: 21,
-          image_url: "../assets/images/orange.jpg"
-        },
-        {
-          name: "Banana",
-          price: 22,
-          image_url: "../assets/images/banana.jpg"
-        },
-        {
-          name: "Grape",
-          price: 23,
-          image_url: "../assets/images/grape.jpg"
-        }
-      ]
-    };
-  },
-  created() {
-    var self = this;
-    self.menuItems.map((x, i) => {
-      self.$set(self.menuItems[i], "highlight", false);
-    });
-    var init = 0;
-    setInterval(function() {
-      if (init === self.menuItems.length) {
-        init = 0;
-      }
-      self.menuItems[init].highlight = true;
-      if (init === 0) {
-        self.menuItems[self.menuItems.length - 1].highlight = false;
-      } else {
-        self.menuItems[init - 1].highlight = false;
-      }
-      init++;
-    }, 2000);
+  components: {
+    ItemsList
   }
 };
 </script>
-
-<style scoped>
-.highlight {
-  background-color: grey;
-}
-</style>
